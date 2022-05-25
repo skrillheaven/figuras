7fadaa841276b74cb300f7471c6fd9a228a43338
skrillheaven <beto.jacp2010@gmail.com>
2022-05-25 16:45:19 -0500

    Agregar archivos de proyecto.

---
+++ figuras/figuras/Cuadrado.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppFigGeom
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Cuadrado : ContentPage
+    {
+
+        int l;
+        int a;
+        double resultado;
+        public Cuadrado()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCuadrado_Clicked(object sender, EventArgs e)
+        {
+            l = int.Parse(input1.Text);
+            a = 0;
+            resultado = Math.Pow(l, 2);
+
+            lblResultado.Text = "El area es: "+resultado + "";
+
+
+
+        }
+    }
+}
---
+++ figuras/figuras/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using AppFigGeom;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace figuras
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Cuadrado_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cuadrado());
+
+        }
+
+        private async Task Triangulo_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Triangulo());
+        }
+
+        private async Task Rectangulo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Rectangulo());
+        }
+
+        private async Task Circulo_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Circulo());
+        }
+    }
+}
