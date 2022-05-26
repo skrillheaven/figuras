991797016b12840db43ffff19a89493febefc01b
skrillheaven <beto.jacp2010@gmail.com>
2022-05-26 17:15:49 -0500

    paguina del rectangulo

--- figuras/figuras/Rectangulo.xaml.cs
+++ figuras/figuras/Rectangulo.xaml.cs
@@ -12,9 +12,25 @@ namespace AppFigGeom
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Rectangulo : ContentPage
     {
+
+        int base1;
+        int altura;
+        double area;
         public Rectangulo()
         {
             InitializeComponent();
+        }
+
+        private void BtnRectangulo_Clicked(object sender, EventArgs e)
+        {
+
+            base1 = int.Parse(input1.Text);
+            altura = int.Parse(input2.Text);
+
+            area = (base1 * altura);
+
+            lblResultado.Text = "El area del rectangulo es: " + area;
+
         }
     }
 }
